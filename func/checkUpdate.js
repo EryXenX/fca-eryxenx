3cf6e80f39a292fadf0566f737ed5e6c99e7c639
Mohammad Akash <eryxenx@yahoo.com>
2026-06-04 16:39:51 +0600

    Delete via GitDrop

--- func/checkUpdate.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Auto-update disabled in fca-eryxenx
-function checkAndUpdateVersion(callback) {
-  if (typeof callback === "function") callback(null);
-  return Promise.resolve();
-}
-module.exports = { checkAndUpdateVersion };
